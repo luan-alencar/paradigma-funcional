ab2361672a1b06ad442246c53e94340cc4a8a3e6
Luan-Augusto <luanalencar134@gmail.com>
2020-10-11 14:26:37 -0300

    primeiro commit

---
+++ src/david/augusto/luan/loop/IteracaoExemplo1.java
@@ -0,0 +1,14 @@
+package david.augusto.luan.loop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IteracaoExemplo1 {
+
+	public static void main(String[] args) {
+
+		List<String> valores = Arrays.asList("1", "2", "3");
+
+	}
+}
