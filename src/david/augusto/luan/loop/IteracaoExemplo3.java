d1c183b33b0bfad0b16bfbd7455c4fa2f03bb34b
Luan-Augusto <luanalencar134@gmail.com>
2020-10-11 15:20:13 -0300

    adicionando comentarios explicativos

--- src/david/augusto/luan/loop/IteracaoExemplo3.java
+++ src/david/augusto/luan/loop/IteracaoExemplo3.java
@@ -6,10 +6,15 @@ import java.util.List;
 public class IteracaoExemplo3 {
 
 	public static int exemplo3() {
+		// criacao da lista de string com seus respectivos elementos
 		List<String> valores = Arrays.asList("5", "10", "20"); // 35
+		// utilizando da Java API stream() - surgiu a partir da edicao 8
 		final int soma = valores.stream()
+				// convertendo um valor inteiro para String com o metodo map()
 				.map(item -> Integer.parseInt(item))
+				// somando os elementos com o metodo reduce()
 				.reduce(0, (a, b) -> a + b);
+		// retornando o resultado do encapsulamento da API Java stream()
 		return soma;
 	}
 }
