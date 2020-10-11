8f5e04f4ffdfc11e1e3c6337f5779da6b0040101
Luan-Augusto <luanalencar134@gmail.com>
2020-10-11 14:35:59 -0300

    comentarios

--- src/david/augusto/luan/loop/IteracaoExemplo1.java
+++ src/david/augusto/luan/loop/IteracaoExemplo1.java
@@ -9,10 +9,14 @@ public class IteracaoExemplo1 {
 
 	public static void main(String[] args) {
 
+		// criacao de strings
 		List<String> valores = Arrays.asList("1", "2", "3");
+		// encapsulando a interacao
 		Iterator<String> it = valores.iterator();
 		int soma = 0;
+		// verifica se existem mais elementos para serem iterados
 		while (it.hasNext()) {
+			// soma entre as strings
 			soma += Integer.parseInt(it.next());
 		}
 
