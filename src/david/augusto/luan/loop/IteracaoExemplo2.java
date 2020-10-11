849c2701b7b383d1a20c27fda465d8209d5d1a1c
Luan-Augusto <luanalencar134@gmail.com>
2020-10-11 15:09:56 -0300

    comentarios

--- src/david/augusto/luan/loop/IteracaoExemplo2.java
+++ src/david/augusto/luan/loop/IteracaoExemplo2.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class IteracaoExemplo2 {
 
 	public static int somaValores() {
-
+		// criacao do array cm seus valores
 		List<String> valores = Arrays.asList("1", "2", "3");
-		int soma = 0;
+		int soma = 0; // variavel para retorna o resultado ao final
+		// forEach para somar cada elemento
 		for (String total : valores) {
+			// convertendo string para inteiro
 			soma += Integer.parseInt(total);
 		}
 		return soma;
