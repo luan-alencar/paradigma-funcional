9fe22c53d2cc055c13e2d1f7050350c3f2751ec0
Luan-Augusto <luanalencar134@gmail.com>
2020-10-11 14:30:58 -0300

    somando elementos

--- src/david/augusto/luan/loop/IteracaoExemplo1.java
+++ src/david/augusto/luan/loop/IteracaoExemplo1.java
@@ -2,6 +2,7 @@ package david.augusto.luan.loop;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class IteracaoExemplo1 {
@@ -9,6 +10,12 @@ public class IteracaoExemplo1 {
 	public static void main(String[] args) {
 
 		List<String> valores = Arrays.asList("1", "2", "3");
+		Iterator<String> it = valores.iterator();
+		int soma = 0;
+		while (it.hasNext()) {
+			soma += Integer.parseInt(it.next());
+		}
 
+		System.out.println("Soma: " + soma);
 	}
 }
